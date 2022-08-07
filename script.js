b51d62f9fcf8e53532dd09fe9ba722cf00897f31
natsu <nyny.sugar@gmail.com>
2022-08-07 16:43:17 +0900

    headerの配置変更

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const menuBtn = document.getElementById("menu-btn");
+
+menuBtn.onclick = function (e) {
+  e.preventDefault();
+
+  document.getElementById("gnavi").classList.toggle("open");
+
+  this.classList.toggle("close");
+};
