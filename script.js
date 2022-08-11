bfb6b6ab474cd4073bc2562a018b417b096160a0
natsu <nyny.sugar@gmail.com>
2022-08-11 23:40:06 +0900

    top画の高さ設定

--- script.js
+++ script.js
@@ -42,9 +42,7 @@ if (windowwidth > 768) {
 }
 
 //Vegas全体の設定
-
 $("#slider").vegas({
-  overlay: true, //画像の上に網線やドットのオーバーレイパターン画像を指定。
   transition: "blur", //切り替わりのアニメーション。http://vegas.jaysalvat.com/documentation/transitions/参照。fade、fade2、slideLeft、slideLeft2、slideRight、slideRight2、slideUp、slideUp2、slideDown、slideDown2、zoomIn、zoomIn2、zoomOut、zoomOut2、swirlLeft、swirlLeft2、swirlRight、swirlRight2、burnburn2、blurblur2、flash、flash2が設定可能。
   transitionDuration: 800, //切り替わりのアニメーション時間をミリ秒単位で設定
   delay: 10000, //スライド間の遅延をミリ秒単位で。
@@ -53,6 +51,11 @@ $("#slider").vegas({
   slides: responsiveImage, //画像設定を読む
   //timer:false,// プログレスバーを非表示したい場合はこのコメントアウトを外してください
 });
+
+function removeattr() {
+  let box = document.getElementById("slider");
+  box.removeAttr("style");
+}
 
 ScrollReveal().reveal(".fadeIn", {
   duration: 1200,
